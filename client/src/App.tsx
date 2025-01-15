09638ad476cc375619e6c787313773182853510c
potakaaa <helbirog@gmail.com>
2025-01-15 21:44:45 +0800

    connected(client and server): django api now sends json on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log(import.meta.env.VITE_API_URL);
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}posts/`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+    } catch (error) {
+      console.error("There was a problem with your fetch operation:", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-zinc-800 w-full h-screen flex justify-center items-center">
+      <p className="font-bold text-4xl text-white">{data[0].id}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
